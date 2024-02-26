5cb9f550211a0ae39ab123da741613ae656e7c51
Brian Ramsay <brian@foont.net>
2024-02-26 23:14:46 +0000

    Updating database works

---
+++ data.sql
@@ -0,0 +1,3 @@
+INSERT INTO student
+VALUES (1,'Finn Bledsoe', 'B0230300','Junior'),
+(2,'Karina Agliullova','B0302300','Senior');
---
+++ update_database.sh
@@ -0,0 +1,19 @@
+psql -c "DROP table student;drop table course; drop table department;drop table migratehistory"
+
+rm -rf migrations
+rm -rf migrations.json
+
+pem init
+
+#pem add app.models.[filename].[classname]
+pem add flaskapp.models.Department
+pem add flaskapp.models.Course
+pem add flaskapp.models.Student
+
+pem watch
+pem migrate
+
+rm -rf migrations
+rm -rf migrations.json
+
+< data.sql psql
